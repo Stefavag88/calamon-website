f48a4bcdd543aa4237d854ecd7f71a9f2aca7de4
Vangelis Stefanakis <stefanakisvag@gmail.com>
2025-08-24 21:32:31 +0300

    restructure index, use astro images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import { imageService } from "@unpic/astro/service";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+      layout: "constrained",
+    }),
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
